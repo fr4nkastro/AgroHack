fbebdb1c9d9ef2a6ac7ffb6eea7e142a43b17fa7
fr4nkastro <frankastro_@outlook.com>
2023-03-24 01:47:59 -0600

    added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ioniclot',
+    appId: '1:319580632096:web:3e9bb389e9d5c7dd1f7845',
+    storageBucket: 'ioniclot.appspot.com',
+    apiKey: 'AIzaSyDZaBLHjOvgsjCPc5dm3GupmrxFsr1ssyY',
+    authDomain: 'ioniclot.firebaseapp.com',
+    messagingSenderId: '319580632096',
+    measurementId: 'G-YN4SR9FDQM',
+  },
   production: true
 };
