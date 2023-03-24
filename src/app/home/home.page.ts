62b73c7590a11d045893b59c36b8545ccbcfbc32
fr4nkastro <frankastro_@outlook.com>
2023-03-24 01:47:49 -0600

    added firestore

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,7 +8,6 @@ import { interval } from 'rxjs';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { AngularFireDatabase } from '@angular/fire/database';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
